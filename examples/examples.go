d09637a8701237d6a65ae8f645bd08b4c5337983
mrz1836 <mrz1818@gmail.com>
2019-10-26 21:22:53 -0400

    A working example to run

---
+++ examples/examples.go
@@ -0,0 +1,29 @@
+/*
+Package main is an example package using go-polynym
+*/
+package main
+
+import (
+	"log"
+
+	"github.com/mrz1836/go-polynym"
+)
+
+func main() {
+
+	// Start a new client
+	client, err := polynym.NewClient()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Resolve a handle
+	var resp *polynym.AddressResponse
+	resp, err = client.ResolveAddress("mrz@moneybutton.com")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Success
+	log.Println("address:", resp.Address)
+}
